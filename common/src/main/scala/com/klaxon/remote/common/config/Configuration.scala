33a42018276af0a65dc4147c61a3debb27839548
Klaxon <skatyg12@gmail.com>
2014-03-01 13:03:56 +0200

    Added Main class of server, which starts the server itself

---
+++ common/src/main/scala/com/klaxon/remote/common/config/Configuration.scala
@@ -0,0 +1,11 @@
+package com.klaxon.remote.common.config
+
+/**
+ * <p>date 3/1/14 </p>
+ * @author klaxon
+ */
+object Configuration {
+
+  val PORT = 6565
+
+}
---
+++ server/src/main/scala/com/klaxon/remote/server/Main.scala
@@ -0,0 +1,61 @@
+package com.klaxon.remote.server
+
+import com.klaxon.remote.common.io.socket.SocketServer
+import java.net.{UnknownHostException, InetAddress, InetSocketAddress}
+import com.klaxon.remote.common.config.Configuration
+
+
+/**
+ * <p>date 2/25/14 </p>
+ * @author klaxon
+ */
+object Main {
+
+  private val HORIZONTAL_LINE = "-" * 40
+
+  def main(args: Array[String]): Unit = {
+    if (args.isEmpty) {
+      println("Please provide host name in command line arguments")
+      return
+    }
+
+    val host = hostFrom(args(0))
+    if (host == None) {
+      println("Unknown host provided")
+      return
+    }
+
+    printlnWithLine("Server start")
+    startServer(host.get)
+  }
+
+  private def hostFrom(arg: String): Option[InetAddress] = {
+    try {
+      Some(InetAddress.getByName(arg))
+    } catch {
+      case e: UnknownHostException => None
+    }
+  }
+
+  private def startServer(host: InetAddress): Unit = {
+    val address = new InetSocketAddress(host, Configuration.PORT)
+    val server = new SocketServer(address, new DeviceManipulator())
+
+    while (true) {
+      println("Press [q] to stop server")
+      val char = readChar()
+      if (char.toLower == 'q') {
+        server.close()
+        printlnWithLine("Server closed")
+        return
+      }
+    }
+  }
+
+  private def printlnWithLine(x: Any) = {
+    println(HORIZONTAL_LINE)
+    println(x)
+    println(HORIZONTAL_LINE)
+  }
+
+}
